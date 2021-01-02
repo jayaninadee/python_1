8973cef55ca36e57fbf3372b7889b42f1e150fed
jayaninadee <jayaninadeeshaa@gmail.com>
2021-01-02 10:15:52 +0530

    NIC

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask import request
+from nic_parser.parser import Parser
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-        return "Hello World 2"
+        name =f"{request.args['name','nic']}"
+        print(f"Name is {name}")
+
+        nic = Parser(name[1])
+        print(f"{nic.birth_date}")
+        print(f"{nic.gender}")
+        print(f"NIC is {nic}")
+        return f"Hello {name} <break> "
 
 if __name__ == '__main__':
     app.run(port=3232)
