6c28925d0903555d97b865d4cac57be251f66653
jayaninadee <jayaninadeeshaa@gmail.com>
2021-01-02 10:18:27 +0530

    NIC  using bd finder

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-        name =f"{request.args['name','nic']}"
-        print(f"Name is {name}")
+    print(f"{request.args}")
+    name = f"{request.args['name']}"
+    nic = f"{request.args['nic']}"
 
-        nic = Parser(name[1])
-        print(f"{nic.birth_date}")
-        print(f"{nic.gender}")
-        print(f"NIC is {nic}")
-        return f"Hello {name} <break> "
+    print(f"Name is {name}")
+    print(f"Nic is {nic}")
+
+    dob = Parser(f"{nic}").birth_date
+    gender = Parser(f"{nic}").gender
+    print(dob)
+    print(gender)
+
+    return f"Dob : {dob} , Gender : {gender.name}"
+
 
 if __name__ == '__main__':
     app.run(port=3232)
-    print("Hello World")
