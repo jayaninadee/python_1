29858abbcfcef0ea4ce403fd91e0f712119899ee
jayaninadee <jayaninadeeshaa@gmail.com>
2021-01-02 10:26:25 +0530

    index String

--- main.py
+++ main.py
@@ -7,20 +7,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    print(f"{request.args}")
-    name = f"{request.args['name']}"
-    nic = f"{request.args['nic']}"
+    index_file=open("templates/index.html","r")
+    index_string=index_file.readline()
+    return f"{''.join(index_string)}"
 
-    print(f"Name is {name}")
-    print(f"Nic is {nic}")
-
-    dob = Parser(f"{nic}").birth_date
-    gender = Parser(f"{nic}").gender
-    print(dob)
-    print(gender)
-
-    return f"Dob : {dob} , Gender : {gender.name}"
-
+    # print(f"{request.args}")
+    # name = f"{request.args['name']}"
+    # nic = f"{request.args['nic']}"
+    #
+    # print(f"Name is {name}")
+    # print(f"Nic is {nic}")
+    #
+    # dob = Parser(f"{nic}").birth_date
+    # gender = Parser(f"{nic}").gender
+    # print(dob)
+    # print(gender)
+    #
+    # return f"Dob : {dob} , Gender : {gender.name}"
+    #
 
 if __name__ == '__main__':
     app.run(port=3232)
